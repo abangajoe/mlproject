a8ec90bc8aa8b8d8321f8754ce2378cf9680e86d
Abangajoe <abangajoe225@gmail.com>
2024-05-09 14:32:09 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYP_E = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+        
+        This function will return a list of requirements
+        
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','')for req in requirements]
+        
+        for HYP_E in requirements:
+            requirements.remove(HYP_E)
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='abangajoe',
+    author_email='abangajoe225@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
